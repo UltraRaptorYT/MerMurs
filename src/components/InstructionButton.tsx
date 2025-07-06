106109a92c33383cfd36ccd22e091d66978f88b1
Soh Hong Yu <sohhongyu@gmail.com>
2025-07-06 23:33:30 +0800

    Update InstructionButton.tsx

--- src/components/InstructionButton.tsx
+++ src/components/InstructionButton.tsx
@@ -9,16 +9,13 @@ import {
 } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 import { Info } from "lucide-react";
-import { HTMLAttributes } from "react";
+import React from "react";
 import Instructions from "@/components/Instructions";
-import ProfilePictureSelector from "@/components/ProfilePictureSelector";
-
-interface InstructionButtonProps extends HTMLAttributes<HTMLButtonElement> {}
 
 export default function InstructionButton({
   className,
   ...rest
-}: InstructionButtonProps) {
+}: React.HTMLAttributes<HTMLButtonElement>) {
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -28,7 +25,7 @@ export default function InstructionButton({
           className={cn(className, "absolute top-3 right-2.5")}
           {...rest} // Spread remaining props like id, onClick, style
         >
-          <Info className="size-8" strokeWidth={2.5}/>
+          <Info className="size-8" strokeWidth={2.5} />
         </Button>
       </DialogTrigger>
       <DialogContent>
